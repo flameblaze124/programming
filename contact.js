1372456c896d264e6dd5b7e96d01d35ae0227a87
flameblaze124 <flameblaze124@gmail.com>
2021-06-18 11:10:14 +1000

    new changes

--- contact.js
+++ contact.js
@@ -9,26 +9,14 @@ function validateForm() {
     if (phone == "") {
         alert("Phone need be filled out");
         return false;
-
     }
+
     var email = document.forms["contact-form"]["email"].value;
     if (email == "") {
         alert("Email need be filled out");
-        return false;
-    } else {
-        return validateEmail(email);
+        return false
     }
-
-    if (form_checked == true) {
-        alert("thank you visting hope you a nice day")
-    }
-}
-
-function validateEmail(value) {
-    if (/^[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) {
+    else {
+        alert("Thank you visting Ulmito Real Estate");
         return true;
-    } else {
-        alert('You have entered an invalid email address.')
-        return false;
-    }
-}
+    }}
